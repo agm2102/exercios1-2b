7accd1a4778e52301fda4d5e691e743398e2d9ec
Adrian Gabriel Moraes <97256008+agm2102@users.noreply.github.com>
2024-08-06 21:59:45 -0300

    Add files via upload

--- exercicios01/04/script.js
+++ exercicios01/04/script.js
@@ -9,29 +9,28 @@ let btCalcular = document.querySelector("#btCalcular");
 let darkLightModeBtn = document.querySelector(".darkLightModeBtn");
 let darkLightModeImg = document.getElementById("darkLightModeImg");
 let container = document.getElementById("container");
-let isDarkMode = false;
 
-function mediaAritmetica() {
-    let res = (Number(valor1.value) + Number(valor2.value) + Number(valor3.value)) / 3;
-    resAritmetica.textContent = res.toFixed(2);
+function calcularMediaAritmetica() {
+    let mediaAritimetica = (Number(valor1.value) + Number(valor2.value) + Number(valor3.value)) / 3;
+    resAritmetica.textContent = mediaAritimetica.toFixed(2);
 
-    return res;
+    return mediaAritimetica;
 }
-function mediaPonderada() {
-    let res = ((Number(valor1.value) * 3) + (Number(valor2.value) * 2) + (Number(valor3.value) * 5)) / 10;
-    resPonderada.textContent = res.toFixed(2);
+function calcularMediaPonderada() {
+    let mediaPonderada = ((Number(valor1.value) * 3) + (Number(valor2.value) * 2) + (Number(valor3.value) * 5)) / 10;
+    resPonderada.textContent = mediaPonderada.toFixed(2);
 
-    return res;
+    return mediaPonderada;
 }
 function somarMedias() {
-    let res = mediaAritmetica() + mediaPonderada();
-    resSomaMedias.textContent = res.toFixed(2);
+    let somaDasMedias = calcularMediaAritmetica() + calcularMediaPonderada();
+    resSomaMedias.textContent = somaDasMedias.toFixed(2);
 
-    return res;
+    return somaDasMedias;
 }
-function mediaDasMedia() {
-    let res = (mediaAritmetica() + mediaPonderada() + somarMedias()) / 3;
-    resMediasDasMedias.textContent = res.toFixed(2);
+function calcularMediaDasMedias() {
+    let mediaDasMedias = (calcularMediaAritmetica() + calcularMediaPonderada() + somarMedias()) / 3;
+    resMediasDasMedias.textContent = mediaDasMedias.toFixed(2);
 }
 
 function verificaSeTemLetras() {
@@ -48,31 +47,15 @@ function verificaSeTemLetras() {
 }
 btCalcular.addEventListener('click', () => {
     if (!verificaSeTemLetras()) {
-        mediaDasMedia();
+        calcularMediaDasMedias();
     }
-})
-btCalcular.addEventListener('mouseover', () => {
-
-    if (!isDarkMode) {
-        btCalcular = document.getElementById("btCalcular").style.boxShadow = "4px 3px 11px 1px greenyellow"
-        btCalcular = document.getElementById("btCalcular").style.color = "greenyellow"
-    }
-    else {
-        btCalcular = document.getElementById("btCalcular").style.boxShadow = "4px 3px 11px 1px rgb(47, 172, 255)"
-        btCalcular = document.getElementById("btCalcular").style.color = "rgb(47, 172, 255)"
-    }
-
-})
-btCalcular.addEventListener('mouseout', () => {
-    btCalcular = document.getElementById("btCalcular").style.boxShadow = "3px 2px 10px 4px black"
-    btCalcular = document.getElementById("btCalcular").style.color = "aliceblue"
 })
 document.addEventListener('keydown', (evento) => {
 
     const keyName = evento.key;
     if (keyName == 'Enter') {
         if (!verificaSeTemLetras()) {
-            mediaDasMedia();
+            calcularMediaDasMedias();
         }
     }
 
@@ -81,16 +64,12 @@ darkLightModeBtn.addEventListener('click', () => {
 
     if (darkLightModeImg.getAttribute('src') == 'images/light_mode_48dp.png') {
         darkLightModeImg.setAttribute('src', 'images/dark_mode_48dp.png');
-        darkLightModeBtn.setAttribute('title', 'Modo Escuro')
         document.body.style.backgroundImage = "url('images/pexels-francesco-ungaro-1525041.jpg')"
         container.style.color = "greenyellow"
-        isDarkMode = false;
     }
     else {
         darkLightModeImg.setAttribute('src', 'images/light_mode_48dp.png');
-        darkLightModeBtn.setAttribute('title', 'Modo Claro')
         document.body.style.backgroundImage = "url('images/pexels-eberhardgross-1421903.jpg')"
         container.style.color = "rgb(47, 172, 255)"
-        isDarkMode = true;
     }
 })
